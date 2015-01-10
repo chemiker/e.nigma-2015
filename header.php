chemiker/e.nigma-2015

commit a527e9c6a8bedd8360681a7f895e1a9836d64003

Refactoring of the top Navigation
* Replaced jquery.popover by Twitter Bootstrap (Due to bugs)
* Added a wrapper for the read-more link

header.php
@@ -15,12 +15,12 @@
 	<nav>
 		<a id="hamburger" tabindex="0" data-icon="&#58887;" class="enigma-icon"><span class="screen-reader"><?php _e('Navigation', 'enigma-2015'); ?></span></a>
 		<ul id="topnavigation"><li>
-				<a href="#information" id="information_link" data-icon="&#58895;" class="enigma-icon"></a>
+				<a href="#information" id="information_link" data-icon="&#58895;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Information', 'enigma-2015'); ?></span></a>
 			</li><li>
-				<a href="#" id="tag_link" data-icon="&#58884;" class="enigma-icon"></a>
-			</li><li><a href="#" id="category_link" data-icon="&#58897;" class="enigma-icon"></a>
+				<a href="#" id="tag_link" data-icon="&#58884;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Tags', 'enigma-2015'); ?></span></a>
+			</li><li><a href="#" id="category_link" data-icon="&#58897;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Category', 'enigma-2015'); ?></span></a>
 			</li><li>
-				<a href="#" id="search_link" data-icon="&#58902;" class="enigma-icon"></a>
+				<a href="#" id="search_link" data-icon="&#58902;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Search', 'enigma-2015'); ?></span></a>
 			</li>
 		</ul>
 	</nav>
@@ -43,7 +43,7 @@
 	</div>
 	<div id="tag-popover" class="popover">
 		<div class="popover-header">
-			Tags
+			<?php _e('Tags', 'enigma-2015'); ?>
 		</div>
 		<div class="popover-content tag-cloud">
 		   	<?php 

functions.php
@@ -40,6 +40,11 @@ add_action( 'comment_form_before', function () {
 	wp_enqueue_script( 'comment-reply' );
 });
 
+// Add a wrapper for the "Read more" link
+add_action( 'the_content_more_link', function ($link) {
+	return "<div class='center'>" . $link . "</div>";
+}, 10, 2 );
+
 // Add localization
 add_action('init', function () {
 	load_theme_textdomain('enigma-2015', '/' . dirname( plugin_basename( __FILE__ ) ) . '/languages');
@@ -64,7 +69,7 @@ add_filter('wp_link_pages_link', function ($link) {
 // Register Scripts
 function scripts_init() {
 	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'jquery-popover', get_template_directory_uri() . '/js/jquery.popover.js' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js' );
 	wp_enqueue_script( 'jquery-scrollupformenu', get_template_directory_uri() . '/js/jquery.scrollupformenu.js' );
 	wp_enqueue_script( 'jquery-enigma', get_template_directory_uri() . '/js/jquery.enigma.js' );
 }

functions/content.php
@@ -117,7 +117,7 @@
 					$meta = '';
 				break;
 				default :
-					$meta = sprintf('<hr class="before_content" /><span class="screen-reader hidden">%s</span><span class="post-meta-date"><a href="%s">%s</a></span>%s',
+					$meta = sprintf('<hr class="before_content" /><span class="screen-reader hidden">%s</span><span class="post-meta-date"><a href="%s">%s</a></span> %s',
 							 __('Published on:', 'enigma-2015'),
 							 get_the_permalink(),
 							 get_the_date(),

index.php
@@ -13,8 +13,7 @@
 			<?php echo $post_vars['thumbnail'] ?>
 			<?php echo apply_filters( 'the_content', $post_vars['content'] ); ?>
 			<div class="post-meta">
-				<?php echo $post_vars['meta'] ?>
-				<?php if ( comments_open() ) : ?>
+				<?php echo $post_vars['meta'];  if ( comments_open() ) : ?>
 					<span class="post-meta-comments"><?php comments_popup_link(); ?></span>
 				<?php endif; ?>
 			</div>

js/jquery.enigma.js
@@ -2,19 +2,15 @@
 	$( document ).ready( function() {
 		$('#navigationwrapper').scrollUpMenu({'transitionTime': 100});
 
-		$('#search_link').popover({
-			header: '#search-popover > .popover-header',
-			content: '#search-popover > .popover-content'
-		});
+		popovers = ['search', 'tag', 'category'];
+		$(popovers).each( function (index, slug) {
+			register_popover(slug);
+		} );
 
-		$('#tag_link').popover({
-			header: '#tag-popover > .popover-header',
-			content: '#tag-popover > .popover-content'
-		});
-
-		$('#category_link').popover({
-			header: '#category-popover > .popover-header',
-			content: '#category-popover > .popover-content'
+		$('.popover-link').on('click', function (e) {
+			e.preventDefault();
+			$(this).focus(); // For mobile devives
+			$('.popover-link').not(this).popover('hide');
 		});
 
 		$("#hamburger").on( 'focusin click', function() {
@@ -22,7 +18,7 @@
 			$("#topnavigation").show();
 		} );
 
-		$( window ).on( "resize", function() {
+		$(window).on( "resize", function() {
 			$(".format-image").css( 'min-height', 0.45 * $(this).width() + "px" );
 		} );
 
@@ -30,6 +26,18 @@
 
 		handle_submenus();
 	} );
+
+	function register_popover(slug) {
+		$('#' + slug + '_link').popover({
+			html: true,
+			placement: 'bottom',
+			trigger: 'focus',
+			title: $('#' + slug + '-popover > .popover-header').html(),
+			content: function () {
+				return $('#' + slug + '-popover > .popover-content').html();
+			}
+		});
+	};
 
 	function handle_submenus() {
 		$(".menu>li, div.menu>ul>li").each( function () {

js/jquery.popover.js
@@ -1,324 +1,0 @@
-// Modified from version taken -> https://github.com/juggy/jquery-popover
-
-(function($) {
-	$.fn.popover = function(options) {
-		var KEY_ESC = 27;
-		
-		// settings stored options and state
-		var settings = $.extend({
-			id: '',						// id for created popover
-			openEvent: null,	// callback function to be called when popup opened
-			closeEvent: null, // callback function to be called when popup closed
-			offsetX: 0,				// fixed offset to correct popup X position
-			offsetY: 0,				// fixed offset to correct popup Y position
-			zindex: 100000,		// default z-index value
-			padding: 18,			// default settings.padding around popover from document edges
-			closeOnEsc: true, // change to false to disable ESC
-			preventLeft: true,		// pass true to prevent left popover
-			preventRight: true,	// pass true to prevent right popover
-			preventTop: true,		// pass true to prevent top popover
-			preventBottom: false, // pass true to prevent bottom popover
-			live: false						// popover created on live selector
-		}, options || {});
-		
-		// functions to claculate popover direction and position 
-		
-		function calcPopoverDirPossible(button, coord) {
-			var possibleDir = {
-				left: false,
-				right: false,
-				top: false,
-				bottom: false
-			}
-			
-			if (coord.buttonOffset.top + coord.buttonHeight + coord.triangleSize + coord.popoverHeight <= 
-									coord.docHeight - settings.padding) {
-				possibleDir.bottom = true;
-			}
-
-			if (coord.buttonOffset.top - coord.triangleSize - coord.popoverHeight >= settings.padding) {
-				possibleDir.top = true;
-			}
-
-			if (coord.buttonOffset.left + coord.buttonWidth + coord.triangleSize + coord.popoverWidth <= 
-									coord.docWidth - settings.padding) {
-				possibleDir.right = true;
-			}
-
-			if (coord.buttonOffset.left - coord.triangleSize - coord.popoverWidth >= settings.padding) {
-				possibleDir.left = true;
-			}
-			
-			return possibleDir;
-		}
-		
-		function chooseDir(possibleDir) {
-			// remove directions prevented by settings
-			if (settings.preventBottom)
-				possibleDir.bottom = false;
-			if (settings.preventTop)
-				possibleDir.top = false;
-			if (settings.preventLeft)
-				possibleDir.left = false;
-			if (settings.preventRight)
-				possibleDir.right = false;
-
-			// determine default direction if nothing works out
-			// make sure it is not one of the prevented directions
-			var dir = 'right';
-			if (settings.preventRight)
-				dir = 'bottom';
-			if (settings.preventBottom)
-				dir = 'top';
-			if (settings.preventTop)
-				dir = 'left';
-
-			if (possibleDir.right)
-				dir = 'right';
-			else if (possibleDir.bottom)
-					dir = 'bottom';
-			else if (possibleDir.left)
-					dir = 'left';
-			else if (possibleDir.top)
-					dir = 'top';
-			
-			return dir;
-		}
-	
-		function calcPopoverPos(button) {
-			// Set this first for the layout calculations to work.
-			settings.popover$.css('display', 'block');
-	
-			var coord = {
-				popoverDir: 'bottom',
-				popoverX: 0,
-				popoverY: 0,
-				deltaX: 0,
-				deltaY: 0,
-				triangleX: 0,
-				triangleY: 0,
-				triangleSize: 20, // needs to be updated if triangle changed in css
-				docWidth: $(window).width(),
-				docHeight: $(window).height(),
-				popoverWidth: settings.popover$.outerWidth(),
-				popoverHeight: settings.popover$.outerHeight(),
-				buttonWidth: button.outerWidth(),
-				buttonHeight: button.outerHeight(),
-				buttonOffset: button.offset()
-			}
-			
-			// calculate the possible directions based on popover size and button position
-			var possibleDir = calcPopoverDirPossible(button, coord);
-				
-			// choose selected direction
-			coord.popoverDir = chooseDir(possibleDir);
-
-			// Calculate popover top
-			if (coord.popoverDir == 'bottom')
-				coord.popoverY = coord.buttonOffset.top + coord.buttonHeight + coord.triangleSize;
-			else if (coord.popoverDir == 'top')
-				coord.popoverY = coord.buttonOffset.top - coord.triangleSize - coord.popoverHeight;
-			else // same Y for left & right
-				coord.popoverY = coord.buttonOffset.top + (coord.buttonHeight - coord.popoverHeight)/2;
-
-			// Calculate popover left
-			if ((coord.popoverDir == 'bottom') || (coord.popoverDir == 'top')) {
-				
-				coord.popoverX = coord.buttonOffset.left + (coord.buttonWidth - coord.popoverWidth)/2;
-
-				if (coord.popoverX < settings.padding) {
-					// out of the document at left
-					coord.deltaX = coord.popoverX - settings.padding;
-				} else if (coord.popoverX + coord.popoverWidth > coord.docWidth - settings.padding) {
-					// out of the document right
-					coord.deltaX = coord.popoverX + coord.popoverWidth - coord.docWidth + settings.padding;
-				}
-
-				// calc triangle pos
-				coord.triangleX = coord.popoverWidth/2 - coord.triangleSize + coord.deltaX;				
-				coord.triangleY = 0;	
-			}
-			else {	// left or right direction
-				
-				if (coord.popoverDir == 'right')
-					coord.popoverX = coord.buttonOffset.left + coord.buttonWidth + coord.triangleSize;
-				else // left
-					coord.popoverX = coord.buttonOffset.left - coord.triangleSize - coord.popoverWidth;
-				
-				if (coord.popoverY < settings.padding) {
-					// out of the document at top
-					coord.deltaY = coord.popoverY - settings.padding;
-				} else if (coord.popoverY + coord.popoverHeight > coord.docHeight - settings.padding) {
-					// out of the document bottom
-					coord.deltaY = coord.popoverY + coord.popoverHeight - coord.docHeight + settings.padding;
-				}
-
-				// calc triangle pos
-				coord.triangleX = 0;
-				coord.triangleY = coord.popoverHeight/2 - coord.triangleSize + coord.deltaY;				
-			}
-	
-			return coord;
-		}
-		
-		function positionPopover(coord) {
-			// set the triangle class for it's direction
-			settings.triangle$.removeClass("left top right bottom");
-			settings.triangle$.addClass(coord.popoverDir);
-
-			if (coord.triangleX > 0) {
-				settings.triangle$.css('left', coord.triangleX);
-			}
-			
-			if (coord.triangleY > 0) {
-				settings.triangle$.css('top', coord.triangleY);
-			}
-
-			// position popover
-			settings.popover$.offset({
-				top: coord.popoverY - coord.deltaY + settings.offsetY,
-				left: coord.popoverX - coord.deltaX + settings.offsetX
-			});
-	
-			// set popover css and show it
-			settings.popover$.css('z-index', settings.zindex).show();
-		}
-
-		// toggle a button popover. If show set to true do not toggle - always show
-		function togglePopover(button, show) {
-			// if this button popover is already open close it an return
-			if ($.fn.popover.openedPopup && 
-				($.fn.popover.openedPopup.get(0) === button.get(0))) {
-				if (!show)
-					hideOpenPopover();
-				return;
-			}
-	
-			// hide any open popover
-			hideOpenPopover();
-	
-			// clicking triangle will also close the popover
-			settings.triangle$.click(function() {
-				button.trigger('hidePopover') 
-			});
-	
-			// reset triangle
-			settings.triangle$.attr("style", "");
-	
-			// calculate all the coordinates needed for positioning the popover and position it 
-			positionPopover(calcPopoverPos(button));
-
-			//Timeout for webkit transitions to take effect
-			setTimeout(function() {
-				settings.popover$.addClass("active");
-			}, 0);
-	
-			if ($.isFunction(settings.openEvent)) {
-				settings.openEvent(button);
-			}
-				
-			$.fn.popover.openedPopup = button;
-			button.addClass('popover-on');
-			$(document).trigger('popoverOpened');
-		}
-	
-		// hide a button popover
-		function hidePopover(button) {
-			button.removeClass('popover-on');
-			$(document).trigger('popoverClosed');
-			settings.popover$.removeClass("active").attr("style", "").hide();
-			if ($.isFunction(settings.closeEvent)) {
-				settings.closeEvent(button);
-			}
-			$.fn.popover.openedPopup = null;			
-		}
-		
-		// hide the currently open popover if there is one
-		function hideOpenPopover() {
-			if ($.fn.popover.openedPopup != null)
-				$.fn.popover.openedPopup.trigger('hidePopover');			
-		}
-	
-		// build HTML popover
-		settings.popover$ = $('<div class="popover" id="' + settings.id + '">'
-					+ '<div class="triangle"></div>'
-					+ '<div class="header"></div><hr class="popover-rule" />'
-					+ '<div class="content"></div>'
-					+ '</div>').appendTo('body');
-		$('.header', settings.popover$).append($(settings.header).detach());
-		$('.content', settings.popover$).append($(settings.content).detach());
-
-		settings.triangle$ = $('.triangle', settings.popover$);
-
-		// remember last opened popup
-		$.fn.popover.openedPopup = null;	
-
-		// setup global document bindings
-		$(document).unbind(".popover");
-
-		// document click closes active popover		
-		$(document).bind("click.popover", function(event) {
-			if (($(event.target).parents(".popover").length == 0)
-					&& (!$(event.target).hasClass('popover-button'))) {
-				hideOpenPopover();
-			}
-		});
-	
-		// document hidePopover event causes active popover to close
-		$(document).bind("hideOpenPopover.popover", hideOpenPopover);
-		
-		// document Esc key listener
-		var selector = this;
-		if (settings.closeOnEsc) {
-			$(document).bind('keydown', function(event) {
-				if (!event.altKey && !event.ctrlKey && !event.shiftKey
-							&& (event.keyCode == KEY_ESC)) {
-					selector.trigger('hidePopover');
-				}
-			});
-		}		
-
-		// setup callbacks for each popover button in wrapped set & return wrapped set
-		if (!settings.live) {
-			return this.each(function() {
-				var button = $(this);
-				button.addClass("popover-button");
-
-				button.bind('click', function() { 
-					togglePopover(button);
-					return false;
-				});
-
-				button.bind('showPopover', function() { 
-					togglePopover(button, true);
-					return false;
-				});
-
-				button.bind('hidePopover', function() {
-					hidePopover(button);
-					return false;
-				});
-			});
-		}
-		else { // live popover		
-			this.live('click', function(event) {
-				$(event.target).addClass("popover-button");
-				togglePopover($(event.target));
-				return false;
-			});
-
-			this.live('showPopover', function(event) { 
-				$(event.target).addClass("popover-button");
-				togglePopover($(event.target), true);
-				return false;
-			});
-
-			this.live('hidePopover', function(event) {
-				hidePopover($(event.target));
-				return false;
-			});
-			
-			return this;		
-		}
-	};
-})(jQuery);

js/jquery.scrollupformenu.js
@@ -66,7 +66,7 @@
 				//}	
 			}
 			// hide all the popovers (Added)
-			$(".popover").hide(); 
+			$(".popover").hide();
 			$("#hamburger").show();
 			$("#topnavigation").hide();
 			lastScrollTop = scrollTop;

commit 3b904404f353656b6d128839d1bb94169903a58f

Fix: Information link was broken since a527e9c6

header.php
@@ -15,7 +15,7 @@
 	<nav>
 		<a id="hamburger" tabindex="0" data-icon="&#58887;" class="enigma-icon"><span class="screen-reader"><?php _e('Navigation', 'enigma-2015'); ?></span></a>
 		<ul id="topnavigation"><li>
-				<a href="#information" id="information_link" data-icon="&#58895;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Information', 'enigma-2015'); ?></span></a>
+				<a href="#information" id="information_link" data-icon="&#58895;" class="enigma-icon"><span class="screen-reader hidden"><?php _e('Information', 'enigma-2015'); ?></span></a>
 			</li><li>
 				<a href="#" id="tag_link" data-icon="&#58884;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Tags', 'enigma-2015'); ?></span></a>
 			</li><li><a href="#" id="category_link" data-icon="&#58897;" class="enigma-icon popover-link"><span class="screen-reader hidden"><?php _e('Category', 'enigma-2015'); ?></span></a>